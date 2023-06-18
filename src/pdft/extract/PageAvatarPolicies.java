5bf38ac1be4cca790b3428b5abbf4c24c3f04cf3
dimwight <dimwight@gmail.com>
2023-06-18 11:58:18 +0100

    TextAvatar

--- src/pdft/extract/PageAvatarPolicies.java
+++ src/pdft/extract/PageAvatarPolicies.java
@@ -1,9 +1,23 @@
 package pdft.extract;
 
-import facets.core.app.avatar.AvatarPolicies;
+import facets.core.app.SViewer;
+import facets.core.app.avatar.*;
 import facets.core.superficial.app.SSelection;
 
 final class PageAvatarPolicies extends AvatarPolicies{
-
-    public static final int MARGINS = -1;}
+    public static final int MARGINS = -1;
+    @Override
+    public AvatarPolicy viewerPolicy(SViewer viewer, AvatarContent content, PainterSource p) {
+        return new AvatarPolicy() {
+            @Override
+            public Painter[] newViewPainters(boolean selected, boolean active) {
+                return null;
+            }
+            @Override
+            public Painter[] newPickPainters(Object hit, boolean selected) {
+                return null;
+            }
+        };
+    }
+}
 
