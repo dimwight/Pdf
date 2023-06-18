22123b58b08033a02e901bcb21c00531183ad298
dimwight <dimwight@gmail.com>
2023-06-18 11:45:42 +0100

    TextAvatar

--- src/pdft/extract/PageRenderView.java
+++ src/pdft/extract/PageRenderView.java
@@ -1,7 +1,11 @@
 package pdft.extract;
 
+import applicable.TextAvatar;
+import facets.core.app.avatar.AvatarContent;
+import facets.core.app.avatar.Painter;
 import facets.core.app.avatar.PlaneViewWorks;
 import facets.core.superficial.app.SSelection;
+import facets.util.geom.Point;
 import facets.util.geom.Vector;
 import facets.util.shade.Shades;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -9,6 +13,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import java.awt.*;
 
 final class PageRenderView extends PlaneViewWorks {
+
     PageRenderView(PageAvatarPolicies avatars) {
         super("Re&ndering", 0, 0, new Vector(0, 0), avatars);
     }
@@ -25,11 +30,35 @@ final class PageRenderView extends PlaneViewWorks {
         setShowValues(across + PageAvatarPolicies.MARGINS, down + PageAvatarPolicies.MARGINS, new Vector(PageAvatarPolicies.MARGINS, PageAvatarPolicies.MARGINS).scaled(0.5), 1);
     }
 
+    private TextAvatar dummy = new TextAvatar() {
+        @Override
+        public String getText() {
+            return "Hi";
+        }
+        @Override
+        public BoundsBox getBounds() {
+            return new BoundsBox(0, 0, 0, 0);
+        }
+        @Override
+        public Painter newViewPainter(boolean selected) {
+            return null;
+        }
+        @Override
+        public Painter[] newPickPainters(boolean selected) {
+            return new Painter[0];
+        }
+        @Override
+        public Object checkCanvasHit(Point canvasAt, double hitGap) {
+            return null;
+        }
+    };
     public SSelection newViewerSelection() {
         return new SSelection() {
             @Override
             public Object content() {
-                return null;
+                return new AvatarContent[]{
+                        dummy
+                };
             }
 
             @Override
